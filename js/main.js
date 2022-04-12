5d7093b200bfa9c5bee3dd2c45824a103534d0d9
Jude Okagu <juud0010@gmail.com>
2022-04-11 22:22:14 -0400

    progress

--- js/main.js
+++ js/main.js
@@ -32,20 +32,69 @@ function loadSong(song){
 
 function playSong(){
     wrapper.classList.add('play')
+    play.querySelector("span.fa-solid").classList.remove("fa-play")
+    play.querySelector("span.fa-solid").classList.add("fa-pause")
     audio.play()
 }
 function pauseSong(){
     wrapper.classList.remove('play')
+    play.querySelector("span.fa-solid").classList.add("fa-play")
+    play.querySelector("span.fa-solid").classList.remove("fa-pause")
     audio.pause()
+}
+
+function prevSong(){
+songIndex--
+
+// if(songIndex < 0){
+//     songIndex = songs.length-1 
+// }
+songIndex < 0? songIndex = songs.length-1 : null
+
+loadSong(songs[songIndex])
+playSong()
+}
+
+function nextSong(){
+    songIndex++
+
+// if(songIndex > songs.length-1){
+//     songIndex = 0 
+// }
+songIndex > songs.length-1? songIndex = 0 : null
+
+loadSong(songs[songIndex])
+playSong()
+}
+
+function updateProgress(e) {
+    // const currentTime = e.srcElement
+    // const duration = e.srcElement
+
+    const {currentTime,duration}= e.srcElement
+    const progressPercent = (currentTime/duration)*100
+    progress.style.width = `${progressPercent}%`
+
+    console.log('here',progressPercent)
 }
 // Event Listner 
 
 play.addEventListener("click",(isPlaying) => {
     isPlaying = wrapper.classList.contains("play")
 
-    if(!isPlaying){
-        playSong()
-    } else {
-        pauseSong()
-    }
+    !isPlaying ? playSong() : pauseSong()
+
+    // if(!isPlaying){
+    //     playSong()
+    // } else {
+    //     pauseSong()
+    // }
 })
+
+// change Song
+prev.addEventListener("click",prevSong)
+next.addEventListener("click",nextSong)
+
+// Audio Progress
+
+audio.addEventListener("timeupdate",updateProgress)
